32a6ab64ef9c3d9057f835c6c4b40b960f8a53f5
Michael Ernest <michael.ehson@gmail.com>
2021-02-09 18:52:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function saturdayFun(activity="roller-skate") {
 }
 
 const mondayWork = function(activity='go to the office') {
-  return `This Monday, I will ${activity}`
+  return `This Monday, I will ${activity}.`
 }
 
 console.log(mondayWork())
