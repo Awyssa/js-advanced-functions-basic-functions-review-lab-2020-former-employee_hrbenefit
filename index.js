9b2f805f053bf3cef0c1226a35dd28a2a7b906f8
Michael Ernest <michael.ehson@gmail.com>
2021-02-09 19:28:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// function saturdayFun(activity="roller-skate") {
-//   return `This Saturday, I want to ${activity}!`
-// }
-//
-// const mondayWork = function(activity='go to the office') {
-//   return `This Monday, I will ${activity}.`
-// }
-//
-// console.log(mondayWork())
-// console.log(mondayWork('skate'))
-//
-//
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity='go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+console.log(mondayWork())
+console.log(mondayWork('skate'))
+
+
 // function outer(greeting, msg="It's a fine day to learn") { // 2
 //   const innerFunction =  function(name, lang="Python") { // 3
 //     return `${greeting}, ${name}! ${msg} ${lang}`; // 4
@@ -18,7 +18,7 @@
 // }
 //
 // console.log(outer("Hello")
-//
+
 
 function outer(greeting, msg="It's a fine day to learn") {
   const innerFunction =  function(name, lang="Python") {
